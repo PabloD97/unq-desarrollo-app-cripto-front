731d780d09dbb7ef094897bee521bdab87bf1c2d
PabloD97 <pablo97758@gmail.com>
2021-10-22 21:57:17 -0300

    feat: Internationalize APP
    - Se agrego la dependencia i18n para manejar el lenguaje de la pagina
    - Se agrego la carpeta translations para almacenar los lenguajes
    - Se agregaron los lenguajes en-es en sus respectivos archivos

---
+++ src/translations/en/translations.js
@@ -0,0 +1,9 @@
+export const TRANSLATIONS_EN = {
+  register: "Register",
+  submit: "Submit",
+  password: "Password",
+  EmailAddress: "Email address",
+  CheckMeOut: "Check me out",
+  warning: "We'll never share your email with anyone else.",
+  changeLanguage: "Change lenguage"
+};
---
+++ src/translations/es/translations.js
@@ -0,0 +1,9 @@
+export const TRANSLATIONS_ES = {
+  register: "Registrarse",
+  submit: "Enviar",
+  password: "Contraseña",
+  EmailAddress: "Dirección de correo electrónico",
+  CheckMeOut: "Echa un vistazo",
+  warning: "Nunca compartiremos su correo electrónico con nadie más.",
+  changeLanguage: "Cambiar lenguaje"
+};
