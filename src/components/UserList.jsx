8cc963646597f8e5f432c18e829f65d799142be2
PabloD97 <pablo97758@gmail.com>
2021-10-28 22:45:50 -0300

    feat: UserList Component
    - Ahora se muestra el listado de usuarios registrados

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,57 +1,56 @@
-import React, {  useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { getUsers } from "../api/users.api";
-import  Navbar  from "./NavBar";
+import Navbar from "./NavBar";
+import { useTranslation } from "react-i18next";
 
 const UserList = () => {
+  const { t } = useTranslation();
 
-
-    const [user, setUsers] = useState([]);
+  const [user, setUsers] = useState([]);
 
   useEffect(() => {
-    console.log('alive')
     getUsersList();
   }, []);
 
   const getUsersList = () => {
-    getUsers().then((result) => {
-
+    getUsers()
+      .then((result) => {
+        console.log(result)
         setUsers(result.data);
       })
       .catch(console.log);
   };
 
-    return (
-        <>
-        <Navbar/>
-        <h2>Listado de usarios</h2>
-        <div>
+  return (
+    <>
+      <Navbar />
+      <h2>Listado de usarios</h2>
+      <div>
         <table className="table">
           <thead>
             <tr>
-              <th>Id</th>
-              <th>Name</th>
-              <th>LastName</th>
-              <th>Email</th>
-
+              <th>{t("name")}</th>
+              <th>{t("lastname")}</th>
+              <th>{t("numberOfOperations")}</th>
+              <th>{t("reputation")} Points</th>
             </tr>
           </thead>
           <tbody>
-            {user.map(crypto => {
+            {user.map((user) => {
               return (
                 <tr key={user.id}>
                   <td>{user.name}</td>
                   <td>{user.lastName}</td>
-                  <td>{user.email}</td>
-                  <td>{user.cvu}</td>
+                  <td>{user.numberOfOperations}</td>
+                  <td>{user.awardedPoints}</td>
                 </tr>
               );
             })}
           </tbody>
         </table>
       </div>
+    </>
+  );
+};
 
-      </>
-    )
-}
-
-export default UserList ;
+export default UserList;
