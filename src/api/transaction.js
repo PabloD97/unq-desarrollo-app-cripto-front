751a9582093957ad7ce69f2864854f39a61cb4e4
PabloD97 <pablo97758@gmail.com>
2021-11-02 16:18:22 -0300

    feat: transactionCanceled
    - Se agrego el llamado a la api de transacicones para cancelar

--- src/api/transaction.js
+++ src/api/transaction.js
@@ -2,46 +2,48 @@ import axios from "axios";
 
 const host = "localhost:8080";
 
-
-
 export const getTransaction = () => {
+  return axios
+    .get(`http://${host}/transaction`, {
+      headers: { Authorization: "Bearer " + localStorage.getItem("token") },
+    })
+    .then((response) => {
+      return response;
+    })
+    .catch((error) => {
+      alert("no ok");
+    });
+};
+export const getTransactionUser = (user) => {
+  console.log(user);
+  return axios.get(`http://${host}/usertransaction`, {
+    params: user,
+  });
+};
+export const activityToTransaction = (id, email) => {
+  return axios.post(`http://${host}/activitytotransaction`, null, {
+    params: {
+      emailUser: email,
+      idActivity: id,
+    },
+  });
+};
 
+export const confirmTransaction = (id) => {
+  return axios.post(`http://${host}/confirmtransaction`, null, {
+    params: {
+      idTransaction: id,
+    },
+  });
+};
 
-    return  axios.get(`http://${host}/transaction`,{  headers:{'Authorization': 'Bearer ' + localStorage.getItem("token")}})
-        .then((response) => {
-            return response
-        })
-        .catch((error) => {
-            alert("no ok")
-
-        })
-
-
-}
-export const getTransactionUser=(user)=>{
-    console.log(user)
-    return axios.get(`http://${host}/usertransaction`,{
-                                                        params: user
-                                                      })
-}
-export const activityToTransaction=(id, email)=>{
-    return axios.post(`http://${host}/activitytotransaction`,
-         null,{params:{
-        emailUser: email,
-            idActivity:id
-    }       })
-}
-
-export const confirmTransaction=(id)=>{
-    return axios.post(`http://${host}/confirmtransaction`,
-        null,{params:{
-
-                idTransaction:id
-            }       })
-}
-
+export const transactionCanceled = (id) => {
+  return axios.post(`http://${host}/cancelltransaction`, null, {
+    params: { idTransaction: id, },
+  });
+};
 
 export default {
-    getTransaction,
-    getTransactionUser
+  getTransaction,
+  getTransactionUser,
 };
